e531697cc1fb37bdaf5264f6ecd6f9cb0bda1926
b <43749613+devops-bh@users.noreply.github.com>
2022-12-01 21:23:39 +0000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,18 +1,18 @@
 apt-get update
 
-apt-get -y remove unscd
+apt-get --force-yes remove unscd
 
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get --force-yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get update
 
-apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get --force-yes install docker-ce docker-ce-cli containerd.io
 
 groupadd docker
 
