4a45de93f0fd0538545d6d239bc2388eedfa7e43
b <43749613+devops-bh@users.noreply.github.com>
2022-12-01 21:09:12 +0000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,25 +1,25 @@
-sudo apt-get update
+apt-get update
 
-sudo apt-get -y remove unscd
+apt-get -y remove unscd
 
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
-sudo groupadd docker
+groupadd docker
 
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
-sudo addgroup --system docker
+addgroup --system docker
 
-sudo adduser $USER docker
+adduser $USER docker
 
 newgrp docker
