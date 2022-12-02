6e911ab6fd359b3b3316a79d304aae1c4c4b5490
b <43749613+devops-bh@users.noreply.github.com>
2022-12-02 13:32:56 +0000

    Delete copy-docker-to-repo.sh

--- copy-docker-to-repo.sh
+++
@@ -1,6 +0,0 @@
-cp /usr/bin/docker /home/ubuntu/declaritive-pipeline                        
-cp /usr/bin/docker-init /home/ubuntu/declaritive-pipeline                        
-cp /usr/bin/docker-proxy /home/ubuntu/declaritive-pipeline                     
-cp /usr/bin/dockerd /home/ubuntu/declaritive-pipeline                             
-cp /usr/bin/dockerd-rootless-setuptool.sh /home/ubuntu/declaritive-pipeline  
-cp /usr/bin/dockerd-rootless.sh /home/ubuntu/declaritive-pipeline
