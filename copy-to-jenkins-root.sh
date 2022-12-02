0f4eb032546ca45018a67b10c3f14f15c99c96ad
b <43749613+devops-bh@users.noreply.github.com>
2022-12-02 13:33:09 +0000

    Delete copy-to-jenkins-root.sh

--- copy-to-jenkins-root.sh
+++
@@ -1,7 +0,0 @@
-sudo cp /usr/bin/docker /home/ubuntu/jenkins-old-data/     
-sudo cp /usr/bin/docker-init /home/ubuntu/jenkins-old-data/
-sudo cp /usr/bin/docker-proxy /home/ubuntu/jenkins-old-data/
-sudo cp /usr/bin/dockerd /home/ubuntu/jenkins-old-data/    
-sudo cp /usr/bin/dockerd-rootless-setuptool.sh /home/ubuntu/jenkins-old-data/
-sudo cp /usr/bin/dockerd-rootless.sh /home/ubuntu/jenkins-old-data/
-
